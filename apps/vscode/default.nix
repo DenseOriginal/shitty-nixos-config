f02e4d628f135860c65d25af6f4adb869c7e6adb
Anders Kok <ak.kornerup@gmail.com>
2024-03-16 23:18:43 +0100

    Add vscode settings and keybindings

--- apps/vscode/default.nix
+++ apps/vscode/default.nix
@@ -2,9 +2,13 @@
     programs.vscode = {
         enable = true;
         package = pkgs.vscode;
+		userSettings = builtins.fromJSON (builtins.readFile "./settings.json");
+		keybindings = builtins.fromJSON (builtins.readFile "./keybindings.json");
         extensions = with pkgs.vscode-marketplace; [
             eamodio.gitlens
             bbenoist.nix
+			avetis.mono-atom
+			vscode-icons-team.vscode-icons
         ];
     };
 }
